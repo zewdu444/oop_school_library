a2f0ffdb267322b977607ed8a2e0bd439a0bff86
zewdu erkyhun <zewdu444@gmail.com>
2023-04-10 19:34:49 +0300

    Fix linter error

--- student.rb
+++ student.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'person'
 # Student class
 class Student < Person
@@ -9,6 +7,6 @@ class Student < Person
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+    '¯(ツ)/¯'
   end
 end

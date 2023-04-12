6259ad70d9ed76c4ae8a15a0bc1e3ea91d7c8f62
zewdu erkyhun <zewdu444@gmail.com>
2023-04-12 17:04:13 +0300

    Add test cases in main

--- main.rb
+++ main.rb
@@ -1,9 +1,37 @@
 require_relative 'person'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
-person = Person.new(18, 'zewdu erkyhun')
-p person.correct_name
-p capitalized_person = CapitalizeDecorator.new(person)
-p capitalized_person.correct_name
-p capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
-p capitalized_trimmed_person.correct_name
+require_relative 'book'
+require_relative 'rental'
+require_relative 'teacher'
+require_relative 'student'
+require_relative 'classroom'
+
+# create a classsroom
+ comp_1 = Classroom.new('Computer Science')
+#  create a student one
+student_one = Student.new(comp_1, 18, 'Minilik', parent_permission: true)
+# create a student two
+student_two = Student.new(comp_1, 18, 'zewdu', parent_permission: true)
+# create a teacher
+teacher_one = Teacher.new(comp_1, 30, 'Mulu')
+
+# create a book
+book_one = Book.new('The Alchemist', 'Paulo Coelho')
+# create a book
+book_two = Book.new('Atomic Habit', 'James Clear')
+# create a rental
+rental_one = Rental.new('4-12-2023',book_one, student_one)
+# create a rental
+rental_two = Rental.new('4-11-2023',book_two, student_two)
+# create a rental
+rental_three = Rental.new('4-11-2023',book_one, teacher_one)
+
+p "library books rental details are as follows"
+p '------------------------------------------'
+p "1. rental name #{rental_one.person.name}  book #{rental_one.book.title} date of rental #{rental_one.date}"
+p "2. rental name #{rental_two.person.name}   book #{rental_two.book.title} date of rental #{rental_two.date}"
+p "3. rental name #{rental_three.person.name} book #{rental_three.book.title} date of rental #{rental_three.date}"
+
+
+

e402e196a027a45266d11758770d0f0a1acd42e5
zewdu erkyhun <zewdu444@gmail.com>
2023-04-13 15:38:20 +0300

    Add creat rental and list by ID

--- app.rb
+++ app.rb
@@ -16,7 +16,8 @@ class App
   end
 
   def run
-    puts 'Welcome to School Library App!'
+    puts 'Welcome to School Library OOP App!'
+    puts '----------------------------------'
 
     loop do
       puts 'Please choose an option by entering a number:'
@@ -26,14 +27,23 @@ class App
       puts '4 - Create a book'
       puts '5 - Create a rental'
       puts '6 - List all rentals for a given person id'
-
+      puts '7 - Exit'
       option = gets.chomp
 
       case option
       when '1'
+       if(@books.length == 0)
+          puts 'There are no books in the library'
+       else
         list_books
+       end
+
       when '2'
+        if(@people.length == 0)
+          puts 'There are no people in the library'
+        else
         list_people
+        end
       when '3'
         create_person
       when '4'
@@ -115,4 +125,36 @@ class App
     @books << Book.new(title, author)
     puts 'Book created successfully'
   end
+  def create_rental
+    puts 'Select a book from the following list by number'
+    @books.each_with_index do |book, index|
+      puts "#{index}) Title: '#{book.title}', Author: #{book.author}"
+    end
+    book_index = gets.chomp.to_i
+
+    puts
+
+    puts 'Select a person from the following list by number (not id)'
+    @people.each_with_index do |person, index|
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    person_index = gets.chomp.to_i
+
+    puts
+
+    print 'Date: '
+    date = gets.chomp
+
+    @rentals << Rental.new(date, @books[book_index], @people[person_index])
+    puts 'Rental created successfully'
+  end
+ def list_rentals_for_person_id
+    print 'ID of person: '
+    id = gets.chomp.to_i
+
+    puts 'Rentals:'
+    @rentals.each do |rental|
+      puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
+    end
+ end
 end
